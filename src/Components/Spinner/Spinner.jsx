665c9f010206a5138d45bd08b437f504c303ba8c
Shadab <shadab19it@gmail.com>
2025-01-28 15:30:38 +0530

    added new changes

---
+++ src/Components/Spinner/Spinner.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import "./Spinner.css";
+
+const SpinnerWithCard = ({ startTime }) => {
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const currentTime = Date.now();
+      const elapsed = (currentTime - startTime) / 1000;
+      setElapsedTime(elapsed);
+    }, 100);
+
+    return () => clearInterval(timer);
+  }, [startTime]);
+
+  return (
+    <div className='spinner-container'>
+      <div className='spinner'></div>
+      <p className='mt-2'>{elapsedTime.toFixed(1)}s</p>
+    </div>
+  );
+};
+
+export default SpinnerWithCard;
